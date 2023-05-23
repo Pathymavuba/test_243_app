bc0de9563f1fae12eafebbfbc5198f42e958ad89
pathymavuba <mavubapathy@gmail.com>
2023-05-23 18:24:33 +0100

    develop:styliser la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+
 
 function App() {
   return (
-   <div>test_243</div>
+   <Routes>
+    <Route path="/"  element={<Home/>} />
+    <Route path="/list"  element ={<List />} />
+   </Routes>
   );
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./styled.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className='parent'>
         <h1>Bienvenu sur Test_243</h1>
-        <Link to="/">
-            <button>Voir la liste des candidats</button>
+        <Link to="/list">
+            <button className='btn-voir'>Voir la liste des candidats</button>
         </Link>
         
     </div>
